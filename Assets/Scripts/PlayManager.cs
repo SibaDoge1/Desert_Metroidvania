cc0898fe505ac13a02ecd041c843df9f6043e580
KimHoseong <ghtjd000129@naver.com>
2019-02-01 04:21:15 +0900

    맵 임시 적용(A2), 카메라 시스템 완성

---
+++ Assets/Scripts/PlayManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    private static PlayManager instance = null;
+    public static PlayManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    void Start()
+    {
+        PlayerPrefs.SetString("Map", "A2");
+        CameraManager.Instance.ResetMapInfo();
+    }
+
+    void Update()
+    {
+
+    }
+}
+
