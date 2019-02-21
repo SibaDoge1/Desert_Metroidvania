85e2d1f86d6e1f6e332ae0bedb7c8625e62e62b0
Peinilla <33536706+Peinilla@users.noreply.github.com>
2019-02-22 03:34:42 +0900

    튜토리얼 요소 추가 (임시)

---
+++ Assets/Scripts/Tutorial/GuideObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideObject : MonoBehaviour
+{
+
+    public enum keyEnum
+    {
+        right = KeyCode.RightArrow,
+        left = KeyCode.LeftArrow,
+        up = KeyCode.UpArrow,
+        down = KeyCode.DownArrow,
+        shift = KeyCode.LeftShift,
+        space = KeyCode.Space,
+        A = KeyCode.A,
+        S = KeyCode.S
+    };
+    public keyEnum code;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode)code))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
