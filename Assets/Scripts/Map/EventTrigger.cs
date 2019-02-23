d47dd1d6ef913737f59741f52dc99ea49ebe6107
Peinilla <33536706+Peinilla@users.noreply.github.com>
2019-02-23 21:21:34 +0900

    Revert "Revert "Merge pull request #11 from SibaDoge1/KSY_System""
    This reverts commit fc3a3b43fe0dbcbc6fcd3a0a30cf8d34dec8a493.

---
+++ Assets/Scripts/Map/EventTrigger.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : InteractObject
+{
+    public enum EventType
+    {
+        GroundOpen, Elevate
+    }
+    public GameObject Obj;
+    public float openTime;
+    public float openThreshold;
+    private bool isTriggered = false;
+    private float stayTimer;
+    public EventType eventType;
+
+    protected override void Action()
+    {
+        Debug.Log(stayTimer);
+    }
+
+    protected override void Update()
+    {
+        if (stayTimer >= openThreshold && !isTriggered)
+        {
+            switch (eventType)
+            {
+                case EventType.GroundOpen: StartCoroutine(GroundOpenRoutine(0.9f, -4f)); break;
+                case EventType.Elevate: StartCoroutine(ElevateRoutine(0.9f, -4f)); break;
+            }
+            isTriggered = true;
+        }
+        else if (isAtObject)
+        {
+            stayTimer += Time.deltaTime;
+        }
+    }
+    
+
+    protected override void OnTriggerExit2D(Collider2D col)
+    {
+        base.OnTriggerExit2D(col);
+        if (col.CompareTag("Player"))
+        {
+            stayTimer = 0;
+        }
+    }
+
+    IEnumerator GroundOpenRoutine(float toScale, float toPosition)
+    {
+       // CameraManager.Instance.ShakeCam(0.2f, openTime);
+        float timer = 0f;
+        Vector3 defaultScale = Obj.transform.localScale;
+        Vector3 defaultPos = Obj.transform.localPosition;
+        Vector3 scale;
+        Vector3 pos;
+        while (timer < openTime)
+        {
+            CameraManager.Instance.MoveCam(Random.insideUnitCircle * 0.2f + (Vector2)CameraManager.Instance.transform.position);
+            scale = Obj.transform.localScale;
+            pos = Obj.transform.localPosition;
+            scale.x = Mathf.Lerp(defaultScale.x, 0.9f, timer / openTime);
+            pos.x = Mathf.Lerp(defaultPos.x, -4f, timer / openTime);
+            Obj.transform.localScale = scale;
+            Obj.transform.localPosition = pos;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator ElevateRoutine(float toScale, float toPosition)
+    {
+        // CameraManager.Instance.ShakeCam(0.2f, openTime);
+        float timer = 0f;
+        Vector3 defaultScale = Obj.transform.localScale;
+        Vector3 defaultPos = Obj.transform.localPosition;
+        Vector3 scale;
+        Vector3 pos;
+        while (timer < openTime)
+        {
+            CameraManager.Instance.MoveCam(Random.insideUnitCircle * 0.2f + (Vector2)CameraManager.Instance.transform.position);
+            scale = Obj.transform.localScale;
+            pos = Obj.transform.localPosition;
+            scale.x = Mathf.Lerp(defaultScale.x, 0.9f, timer / openTime);
+            pos.x = Mathf.Lerp(defaultPos.x, -4f, timer / openTime);
+            Obj.transform.localScale = scale;
+            Obj.transform.localPosition = pos;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
